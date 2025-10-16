6a77ca94545c3db942d64cd0ad11568e09b29de9
Kai Hotz <kai@liquidity-capital.com>
2025-10-16 16:29:02 +0400

    z-index adjusted

--- src/components/Calendar/CalendarEvent.tsx
+++ src/components/Calendar/CalendarEvent.tsx
@@ -64,14 +64,13 @@ export const CalendarEvent: FC<ICalendarEventProps> = ({
 
     return (
         <div
-            className="absolute rounded px-1 md:px-2 py-1 text-xs text-white overflow-hidden cursor-move group opacity-90 min-h-4"
+            className="absolute rounded px-1 md:px-2 py-1 text-xs text-white overflow-hidden cursor-move group opacity-90 min-h-4 z-10"
             style={{
                 top: `${top}%`,
                 height: `${height}%`,
                 left: `${left}%`,
                 width: `${width}%`,
                 backgroundColor: event.color,
-                zIndex: 10, // Ensure proper stacking of overlapping events
             }}
             onMouseDown={handleDrag}
         >
